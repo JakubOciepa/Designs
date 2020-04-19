0c679d01914d3b9b2f3f56c31711d230a15eb31a
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-04-19 15:47:30 +0200

    Add view with sidebar collapsed

---
+++ Contador/Contador/MainViewSidebarCollapsed.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Contador
+{
+    /// <summary>
+    /// Interaction logic for MainViewSidebarCollapsed.xaml
+    /// </summary>
+    public partial class MainViewSidebarCollapsed : Window
+    {
+        public MainViewSidebarCollapsed()
+        {
+            InitializeComponent();
+        }
+    }
+}

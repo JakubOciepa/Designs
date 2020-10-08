417ad3fa5546981847ee2e1ca2268d2366fc90de
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-08 18:15:14 +0200

    Style the window

---
+++ LoginPage/Views/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using LoginPage.ViewModels;
+using System.Windows;
+
+namespace LoginPage.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ILoginControlViewModel _loginControlViewModel;
+        private readonly IMainContentViewModel _mainContentViewModel;
+        public MainWindow()
+        {
+            _loginControlViewModel = new LoginControlViewModel();
+            _mainContentViewModel = new MainContentViewModel(_loginControlViewModel);
+
+            InitializeComponent();
+
+            DataContext = new MainWindowViewModel(this, _mainContentViewModel);
+        }
+    }
+}

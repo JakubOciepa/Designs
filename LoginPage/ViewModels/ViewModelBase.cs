11cae082c264c2951dead1783c1327b3d8ea966d
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-08 16:54:07 +0200

    Add viewmodelbase

---
+++ LoginPage/ViewModels/ViewModelBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LoginPage.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        protected bool _disposed = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+            }
+        }
+    }
+}
